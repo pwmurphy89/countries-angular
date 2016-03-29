111c7e4dd0bffab16193d0e1e8c094651c47be38
pwmurphy89 <pwmurphy89@gmail.com>
2016-03-29 09:52:28 -0400

    Countries
    More work with Angular

---
+++ scripts.js
@@ -0,0 +1,69 @@
+var myApp = angular.module('myApp', []);
+// console.log(myApp);
+myApp.controller('myController', function($scope){
+	$scope.countries = [
+		{
+			name: "Bolivia",
+			president: "Evo Morales",
+			flag: "red, yellow, green"
+		},
+		{
+			name: "America",
+			president: "Obama",
+			flag: "red, white, blue"
+		},
+		{
+			name: "Peru",
+			president: "Ollanta Humala",
+			flag: "red, white"
+		},
+		{
+			name: "Mexico",
+			president: "Enrique Peña Nieto",
+			flag: "red, green, white"
+		},
+		{
+			name: "China",
+			president: "Xi JinPing",
+			flag: "red, yellow"
+		},
+		{
+			name: "Russia",
+			president: "Putin",
+			flag: "red,white,blue"
+		},
+		{
+			name: "Canada",
+			president: "Justin Trudeau",
+			flag: "red,white"
+		},
+		{
+			name: "France",
+			president: "François Hollande",
+			flag: "red,white,blue"
+		},
+		{
+			name: "Germany",
+			president: "Angela Merkel",
+			flag:"black, red, yellow"
+		},
+		{
+			name: "Sweden",
+			president: "Carl XVI Gustaf of Sweden",
+			flag: "blue, yellow"
+		}
+
+
+	]
+
+	$scope.addCountry = function() {
+		$scope.countries.push(
+
+			{
+				name: $scope.name,
+				president: $scope.president,
+				flag: $scope.flag
+	}
+	)
+	}
+});
